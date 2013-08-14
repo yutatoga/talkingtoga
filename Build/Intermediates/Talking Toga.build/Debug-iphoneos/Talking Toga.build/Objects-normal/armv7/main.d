85ad4a60c7c49b13571f38465fb40894bc463f7b
yutatoga <yuta.toga@gmail.com>
2013-08-15 01:19:34 +0900

    add images and update ib file

---
+++ Build/Intermediates/Talking Toga.build/Debug-iphoneos/Talking Toga.build/Objects-normal/armv7/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/gentleman/Developer/Objective-C/myApps/Talking\ Toga/Talking\ Toga/main.m \
+  /Users/gentleman/Developer/Objective-C/myApps/Talking\ Toga/Talking\ Toga/AppDelegate.h
